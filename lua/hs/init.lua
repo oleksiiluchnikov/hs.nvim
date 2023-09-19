8efeddb82b56bd2890bf8659dbf45d80bd3c1a99
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2023-09-19 19:49:35 +0300

    test

--- lua/hs/init.lua
+++ lua/hs/init.lua
@@ -4,12 +4,21 @@ function M.setup()
     print("Hammerspoon is installed")
 end
 
+-- Metatable to handle dynamic function delegation
 return setmetatable({}, {
     __index = function(_, k)
+        -- Check if method exists in M table
         if M[k] then
             return M[k]
         else
-            return require("hs." .. k)
+            -- Safely try to require from 'hs' namespace
+            local success, module = pcall(require, "hs." .. k)
+            if success then
+                return module
+            else
+                -- Handle error (e.g., log it, or return a default function)
+                return nil
+            end
         end
     end,
 })
