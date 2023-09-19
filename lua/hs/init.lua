5e770a0a742abf0fe65f807365b74b35ea183db6
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2023-09-19 19:58:13 +0300

    test

--- lua/hs/init.lua
+++ lua/hs/init.lua
@@ -1,24 +1,8 @@
 local M = {}
+print("Hammerspoon is loading")
 
 function M.setup()
-    print("Hammerspoon is installed")
+    vim.notify("Hammerspoon is loading")
 end
 
--- Metatable to handle dynamic function delegation
-return setmetatable({}, {
-    __index = function(_, k)
-        -- Check if method exists in M table
-        if M[k] then
-            return M[k]
-        else
-            -- Safely try to require from 'hs' namespace
-            local success, module = pcall(require, "hs." .. k)
-            if success then
-                return module
-            else
-                -- Handle error (e.g., log it, or return a default function)
-                return nil
-            end
-        end
-    end,
-})
+return M
