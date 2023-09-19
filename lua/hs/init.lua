2426d76525de54ced0dd81eca1cd55313d9964b3
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2023-09-19 20:53:58 +0300

    feat: cmd, console, configdir, config, inspect

--- lua/hs/init.lua
+++ lua/hs/init.lua
@@ -4,10 +4,42 @@ function M.setup()
     vim.notify("Hammerspoon is loading")
 end
 
-function M.configdir()
-    local hs_configdir = vim.fn.system("hs -c 'hs.configdir'")
-    local cmd = "edit " .. hs_configdir
-    vim.cmd(cmd)
+function M.cmd(cmd)
+    local hs_cmd = "hs -c '" .. cmd .. "'"
+    local system_obj = vim.fn.systemlist(hs_cmd)
+    return system_obj[1]
+end
+
+function M.console()
+    M.cmd("hs.openConsole()")
+end
+
+local function configdir()
+    local output = M.cmd("hs.configdir()")
+    return output and string.gsub(output, "\n", "")
+end
+
+function M.config()
+    local config_dir = configdir()
+    if config_dir then
+        vim.cmd = "edit " .. config_dir .. "/init.lua"
+    end
+end
+
+function M.inspect(args)
+    local query = args.args
+    local cmd = "hs.inspect.inspect(" .. query .. ")"
+    local output = M.cmd(cmd)
+
+    if output == nil then
+        return
+    end
+
+    if string.len(output) > 1000 then
+        output = string.sub(output, 1, 1000) .. "..."
+    end
+
+    vim.notify(output)
 end
 
 return M
